5bd17f565e77fcb10f80b2bdfaf0c9f220960473
pednaruemol <naruemol.kphet@gmail.com>
2024-01-26 05:43:08 +0700

    finished:exercise1-5 add db.js,connect db in app.js and add APIs in products.js/Creating APIs

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,58 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  const collection = db.collection("products");
+  const allProducts = await collection.find().limit(10).toArray();
+  return res.json({ data: allProducts });
+});
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = new ObjectId(req.params.id);
 
-productRouter.post("/", (req, res) => {});
+  const productById = await collection.findOne({ _id: productId });
 
-productRouter.put("/:id", (req, res) => {});
+  return res.json({ data: productById });
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("products");
+  const newProductData = await collection.insertOne(req.body);
+  return res.json({
+    message: "Product has been created successfully",
+  });
+});
+
+productRouter.put("/:id", async (req, res) => {
+  const collection = db.collection("products");
+
+  const editedProductData = { ...req.body };
+  const productId = new ObjectId(req.params.id);
+
+  await collection.updateOne(
+    {
+      _id: productId,
+    },
+    {
+      $set: editedProductData,
+    }
+  );
+  return res.json({
+    message: "Product has been updated successfully",
+  });
+});
+
+productRouter.delete("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+
+  return res.json({
+    message: "Product has been deleted successfully",
+  });
+});
 
 export default productRouter;
